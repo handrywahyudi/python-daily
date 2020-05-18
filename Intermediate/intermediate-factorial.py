67a26c435d6868dce957876090c7e17620ae12ca
handry wahyudi <handry19@gmail.com>
2020-05-18 23:39:30 +0700

    Create function factorial.

---
+++ Intermediate/intermediate-factorial.py
@@ -0,0 +1,15 @@
+def get_factor(n):
+    factors = list()
+    divisor = 2
+    while(divisor <= n):
+        if (n % divisor) == 0:
+            factors.append(divisor)
+            n = n/divisor
+        else:
+            divisor += 1
+    return factors
+
+
+if __name__ == "__main__":
+    print(get_factor(100))
+    
